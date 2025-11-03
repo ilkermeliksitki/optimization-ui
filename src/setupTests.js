6054189c63dd8b6f77943b5c815b32fe1ccdc370
ilker m. sitki <ilkermeliksitki@proton.me>
2025-11-03 12:42:26 +0100

    test: add ResizeObserver mock for Rechart tests
    Changes to be committed:
    	modified:   src/setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,3 +4,17 @@ import { vi } from 'vitest';
 // Mock window.alert for all tests
 global.alert = vi.fn();
 
+global.ResizeObserver = class ResizeObserver {
+    constructor(callback) {
+        this.callback = callback;
+    }
+    observe() {
+        // Mock observe - do nothing
+    }
+    unobserve() {
+        // Mock unobserve - do nothing
+    }
+    disconnect() {
+        // Mock disconnect - do nothing
+    }
+};
